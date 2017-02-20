f556343317e42a654bdfd04fd049632ed05e9bc8
Meg Dahlgren <meg.dahlgren@gmail.com>
2017-02-20 14:53:52 -0500

    add email template images

--- src/js/reducers/projectReducer.js
+++ src/js/reducers/projectReducer.js
@@ -1,16 +1,18 @@
+import '../components/Portfolio/images/iphone6s-vertical-allowed-1366.png'
+
 function reducer(state = {
     singlePageApps: [
         {
             title: 'Calorie Tracker',
             name: 'calorieTracker',
-            img: '../../../images/health-tracker-resize-50-comp.png',
+            img: '../components/Portfolio/images/health-tracker-resize-50-comp.png',
             source: 'https://github.com/megulus/health-tracker',
             site: 'https://megulus.github.com/health-tracker',
         },
         {
             title: 'Neighborhood Map',
             name: 'neighborhoodMap',
-            img: '../../../images/nbhd-map-resize-50-comp.png',
+            img: '../components/Portfolio/images/nbhd-map-resize-50-comp.png',
             source: 'https://github.com/megulus/neighborhood_map',
             site: 'https://megulus.github.io/neighborhood_map',
         }
@@ -18,17 +20,20 @@ function reducer(state = {
     emailTemplates: [
         {
             title: 'Email Template - Outlook 2011',
-            img: '../../../images/ol2011-vertical-allowed-1366.png',
+            name: 'outlook2011',
+            img: '../components/Portfolio/images/ol2011-vertical-allowed-1366.png',
             source: 'https://github.com/megulus/emailtemplates',
         },
         {
             title: 'Email Template - iPhone 6S Plus',
-            img: '../../../images/iphone6splus-vertical-allowed-1366.png',
+            name: 'iPhone6SPlus',
+            img: '../components/Portfolio/images/iphone6splus-vertical-allowed-1366.png',
             source: 'https://github.com/megulus/emailtemplates',
         },
         {
             title: 'Email Template - iPhone 6',
-            img: '../../../images/iphone6s-vertical-allowed-1366.png',
+            name: 'iPhone6',
+            img: '../components/Portfolio/images/iphone6s-vertical-allowed-1366.png',
             source: 'https://github.com/megulus/emailtemplates',
         }
     ]
