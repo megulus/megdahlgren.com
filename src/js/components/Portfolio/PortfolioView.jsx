214eeae8176ad6804bc9eef356d6a381b5bb8e86
Meg Dahlgren <meg.dahlgren@gmail.com>
2017-02-20 14:35:27 -0500

    add susy grid to Portfolio component

--- src/js/components/Portfolio/PortfolioView.jsx
+++ src/js/components/Portfolio/PortfolioView.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import Image from './Image';
 
-/*import { setProjects } from '../../actions/projectActions';*/
+import grid from '../../../scss/app.scss';
 
 @connect((store) => {
     return {
@@ -15,9 +15,19 @@ import Image from './Image';
 
 class PortfolioView extends Component {
 
+    getListOfDivs(list) {
+        const divs = [];
+        for (let [index, value] of list.entries()) {
+            divs.push(this.getItemDiv(value, index));
+        }
+        return divs;
+    }
+
     getItemDiv(item, i) {
+        const name = 'column' + ((i % 3) + 1);
+        const className = grid[name];
         return (
-            <div key={i}>
+            <div key={i} className={className}>
                 <h3>{item.title}</h3>
                 <Image name={item.name}/>
             </div>
@@ -25,18 +35,28 @@ class PortfolioView extends Component {
     }
 
     render() {
-        const singlePageApps = this.props.singlePageApps;
-        /*console.log(singlePageApps);*/
-        let apps = [];
-        for (let [index, value] of singlePageApps.entries()) {
-            /*console.log(index, value);*/
-            apps.push(this.getItemDiv(value, index));
-        }
+        /*const singlePageApps = this.props.singlePageApps;
+         const emailTemplates = this.props.emailTemplates;
+         let apps = [];
+         let templates = [];
+         for (let [index, value] of singlePageApps.entries()) {
+         apps.push(this.getItemDiv(value, index));
+         }*/
+        const apps = this.getListOfDivs(this.props.singlePageApps);
+        const templates = this.getListOfDivs(this.props.emailTemplates);
         return (
-            <div>
-                <h1>Featured Work</h1>
-                {apps}
+            <div >
+                <div>
+                    <h2 className={grid.row}>Featured Work</h2>
+                    {apps}
+
+                </div>
+                <div>
+                    <h2 className={grid.row}>Email Templates</h2>
+                    {templates}
+                </div>
             </div>
+
         );
     }
 
